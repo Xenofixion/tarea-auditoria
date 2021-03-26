f7f8067f03be46482d9f307a126a4b1c143f05e5
Xenofixion <davila.bojorquez@gmail.com>
2021-03-26 15:28:39 -0600

    @Xenofixion - modificados background del titulo y primera seccion

--- layout/scripts/jquery.backtotop.js
+++ layout/scripts/jquery.backtotop.js
@@ -10,7 +10,7 @@ File: Back to Top JS
 jQuery("#backtotop").click(function () {
     jQuery("body,html").animate({
         scrollTop: 0
-    }, 600);
+    }, 0);
 });
 jQuery(window).scroll(function () {
     if (jQuery(window).scrollTop() > 150) {
